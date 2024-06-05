390a2060eebc95aa54ae4a969bd8b105d86d3d9a
unknown <ugochukwu.ikpeazu@euruni.edu>
2024-06-05 08:02:16 +0300

    updating javascript test

---
+++ javaScriptTest.js
@@ -0,0 +1,6 @@
+// var testWord = "This is a dummy tweet";
+// console.log("You have written",testWord.length," characters. You have", 140 - testWord.length, "characters left");
+
+var myName = "Angela";
+myName.slice(0,3);
+console.log(myName.slice(0,3));
