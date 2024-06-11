d67c2cff5bfb1734c762eb52dc391325f22169a8
unknown <ugochukwu.ikpeazu@euruni.edu>
2024-06-11 08:25:19 +0300

    Updating front end course

--- javaScriptTest.js
+++ javaScriptTest.js
@@ -1,6 +1,14 @@
 // var testWord = "This is a dummy tweet";
 // console.log("You have written",testWord.length," characters. You have", 140 - testWord.length, "characters left");
 
-var myName = "Angela";
-myName.slice(0,3);
-console.log(myName.slice(0,3));
+// var myName = "Angela";
+// myName.slice(0,3);
+// console.log(myName.slice(0,3));
+
+// var dummyTweet = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+//console.log(dummyTweet.length);
+// console.log(dummyTweet.slice(0,141));
+
+var yourName = ("What is your name?: ");
+console.log(yourName.toUpperCase);
+console.log(yourName.toLocaleLowerCase);
