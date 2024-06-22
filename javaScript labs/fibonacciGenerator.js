90942e33c893ef058c27969e84d0b69e0da96e5f
unknown <ugochukwu.ikpeazu@euruni.edu>
2024-06-22 06:58:51 +0300

    JavaScript and HTML

---
+++ javaScript labs/fibonacciGenerator.js
@@ -0,0 +1,16 @@
+//to be updated
+
+function fibonacciGenerator (n) {
+    //var newArray = [];
+    for (var i = 0; i < n; i++){
+        newArray.push(i);
+        
+        //console.log(i);
+        //console.log(i+ (i-1));
+        //var newNumber = i + (i-1);
+        //return newNumber
+    }
+    
+}
+
+console.log(fibonacciGenerator(6));
