c99ec8afe4ad69a597a743c0836b203d063d1b12
unknown <ugochukwu.ikpeazu@euruni.edu>
2024-06-24 07:51:05 +0300

    updating dice challenge

---
+++ 17. Dice challenge/index.js
@@ -0,0 +1,7 @@
+
+var randomNumber1 = Math.floor(Math.random() * 6);
+document.getElementsByClassName("img1")[0].setAttribute("src", ("images/dice" + randomNumber1 + ".png"));
+
+var randomNumber2 = Math.floor(Math.random() * 6);
+document.getElementsByClassName("img2")[0].setAttribute("src", ("images/dice" + randomNumber2 + ".png"));
+
