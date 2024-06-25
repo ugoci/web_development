63b2edaa5d91dc2134d5e53956b12f1cc276379d
unknown <ugochukwu.ikpeazu@euruni.edu>
2024-06-25 07:47:32 +0300

    updating dice game with final steps

--- 17. Dice challenge/index.js
+++ 17. Dice challenge/index.js
@@ -1,7 +1,11 @@
 
-var randomNumber1 = Math.floor(Math.random() * 6);
+var randomNumber1 = Math.floor(Math.random() * 6 + 1);
 document.getElementsByClassName("img1")[0].setAttribute("src", ("images/dice" + randomNumber1 + ".png"));
 
-var randomNumber2 = Math.floor(Math.random() * 6);
+var randomNumber2 = Math.floor(Math.random() * 6 + 1);
 document.getElementsByClassName("img2")[0].setAttribute("src", ("images/dice" + randomNumber2 + ".png"));
 
+var gameWinner = document.firstElementChild.lastElementChild.firstElementChild.firstElementChild;
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 wins!"; 
+}else {document.querySelector("h1").innerHTML = "Player 2 wins!";}
