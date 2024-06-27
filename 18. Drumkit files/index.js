0945ed7eb96d32d2a283816e290fd0127f8c855e
unknown <ugochukwu.ikpeazu@euruni.edu>
2024-06-27 07:18:31 +0300

    starting to work on event listeners with the drum kit

---
+++ 18. Drumkit files/index.js
@@ -0,0 +1,8 @@
+//document.querySelector("button").addEventListener("click", handleClick);
+
+document.getElementsByClassName("w drum")[0].addEventListener("click", handleClick);
+
+function handleClick(){
+    alert("I got clicked");
+}
+
