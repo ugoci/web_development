6119cce97403236454c79ab05ff73787ae070976
unknown <ugochukwu.ikpeazu@euruni.edu>
2024-06-27 07:26:51 +0300

    updating event listeners for all drums

--- 18. Drumkit files/index.js
+++ 18. Drumkit files/index.js
@@ -2,7 +2,19 @@
 
 document.getElementsByClassName("w drum")[0].addEventListener("click", handleClick);
 
+document.getElementsByClassName("a drum")[0].addEventListener("click", handleClick);
+
+document.getElementsByClassName("s drum")[0].addEventListener("click", handleClick);
+
+document.getElementsByClassName("d drum")[0].addEventListener("click", handleClick);
+
+document.getElementsByClassName("j drum")[0].addEventListener("click", handleClick);
+
+document.getElementsByClassName("k drum")[0].addEventListener("click", handleClick);
+
+document.getElementsByClassName("l drum")[0].addEventListener("click", handleClick);
+
+
 function handleClick(){
     alert("I got clicked");
 }
-
