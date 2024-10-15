069601a8d29d1f5adb3b225d2035ccf1a3bc911f
unknown <ugoi@protonmail.ch>
2024-10-15 20:22:12 +0300

    working with middlewares

---
+++ 34. Middleware/index1.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+//these lines of code help the program to determine where a file is hosted
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
